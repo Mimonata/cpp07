3b60f6030be809354b6d7079fe89897054bc0b9d
Mimonata <sabina_pitul@yahoo.de>
2025-07-24 21:11:59 +0200

    d

---
+++ ex01/Iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 21:14:26 by spitul            #+#    #+#             */
+/*   Updated: 2025/07/21 21:28:02 by spitul           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+#include <cstdint>
+
+void	iter(uintptr_t adr, size_t size, )
+
+#endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,33 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/18 20:23:51 by spitul            #+#    #+#             */
-/*   Updated: 2025/07/21 21:03:59 by spitul           ###   ########.fr       */
+/*   Created: 2025/07/21 21:14:23 by spitul            #+#    #+#             */
+/*   Updated: 2025/07/21 21:16:01 by spitul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Whatever.hpp"
-#include <iostream>
-#include <string>
+#include "Iter.hpp"
 
 int	main(void)
 {
-	int	a = 4;
-	int	b = 8;
-	std::string c = "test";
-	std::string d = "sky";
-
-	swap(a, b);
-	std::cout << "Value of a after swap = " << a;
-	std::cout << "\nValue of b after swap = " << b;
-	std::cout << "\nMinimum (a, b) = " << min(a, b);
-	std::cout << "\nMaximum (a, b) = " << max(a, b) << std::endl;
-	
-	swap(c, d);
-	std::cout << "Value of c after swap = " << c;
-	std::cout << "\nValue of d after swap = " << d;
-	std::cout << "\nMinimum (c, d) = " << ::min(c, d);
-	std::cout << "\nMaximum (c,db) = " << ::max(c, d) << std::endl;
-	
 	return 0;
 }
